ae041f98ac326c4695f252d0a7d1eacceb261b4f
Hyurielh <animehyurielh@gmail.com>
2025-09-13 20:45:06 -0600

    Funciones de remover tareas y marcar completadas

--- script.js
+++ script.js
@@ -7,13 +7,32 @@ buttonTask.addEventListener('click', addTask);
 function addTask() {
     console.log('funciona');
     let task = inputTask.value;
+
     if (task === '') {
         alert('Por favor agregue una tarea');
     }
     else{
        let listTask = document.createElement('li');
+       let checklistTask = document.createElement('input');
+       let btnlistTask = document.createElement('button');
+       
+       checklistTask.setAttribute('type', 'checkbox');
+       btnlistTask.textContent = 'Eliminar';
        listTask.textContent = task;
+
+       btnlistTask.addEventListener('click', deleteTask);
+       checklistTask.addEventListener('click', completeTask);
+
        list.append(listTask);
+       listTask.append(checklistTask);
+       listTask.append(btnlistTask);
        inputTask.value = '';
     }
+}
+function deleteTask() {
+    this.parentNode.remove();
+}
+
+function completeTask() {
+    this.parentNode.classList.toggle('taskCompleted');
 }
